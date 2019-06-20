3ee5216b89c3ba112ca60c1f241e3986df23dab6
Elvijs Sarkans <elvijs@prowler.io>
2019-06-20 15:19:56 +0100

    Little workaround

--- tests/pass/records.py
+++ tests/pass/records.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.core.records as records
 
 # Example data from numpy docs
 x1 = np.array([1, 2, 3, 4])
@@ -7,19 +8,18 @@ x3 = np.array([1.1, 2, 3, 4])
 names = ['a', 'b', 'c']
 
 # Testing various parameter types
-np.core.records.fromarrays([x1, x2, x3])
-np.core.records.fromarrays([x1, x2, x3], dtype=None)
-np.core.records.fromarrays([x1, x2, x3],
-                           dtype=[('a', np.int32), ('b', np.str_), ('c', np.float32)])
-np.core.records.fromarrays([x1, x2, x3], shape=None)
-np.core.records.fromarrays([x1, x2, x3], shape=(4,))
-np.core.records.fromarrays([x1, x2, x3], formats=None)
-np.core.records.fromarrays([x1, x2, x3], formats=['i', 'U', 'f'])
-np.core.records.fromarrays([x1, x2, x3], names=None)
-np.core.records.fromarrays([x1, x2, x3], names=names)
-np.core.records.fromarrays([x1, x2, x3], titles=None)
-np.core.records.fromarrays([x1, x2, x3], titles=names)
-np.core.records.fromarrays([x1, x2, x3], aligned=True)
-np.core.records.fromarrays([x1, x2, x3], aligned=False)
-np.core.records.fromarrays([x1, x2, x3], byteorder=None)
-np.core.records.fromarrays([x1, x2, x3], byteorder='L')
+records.fromarrays([x1, x2, x3])
+records.fromarrays([x1, x2, x3], dtype=None)
+records.fromarrays([x1, x2, x3], dtype=[('a', np.int32), ('b', np.str_), ('c', np.float32)])
+records.fromarrays([x1, x2, x3], shape=None)
+records.fromarrays([x1, x2, x3], shape=(4,))
+records.fromarrays([x1, x2, x3], formats=None)
+records.fromarrays([x1, x2, x3], formats=['i', 'U', 'f'])
+records.fromarrays([x1, x2, x3], names=None)
+records.fromarrays([x1, x2, x3], names=names)
+records.fromarrays([x1, x2, x3], titles=None)
+records.fromarrays([x1, x2, x3], titles=names)
+records.fromarrays([x1, x2, x3], aligned=True)
+records.fromarrays([x1, x2, x3], aligned=False)
+records.fromarrays([x1, x2, x3], byteorder=None)
+records.fromarrays([x1, x2, x3], byteorder='L')
