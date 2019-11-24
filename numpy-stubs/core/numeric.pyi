1b7afd7c9e4c850462c48f7bbb2d77ee33900de3
Johan Vergeer <johanvergeer@gmail.com>
2019-11-24 20:44:13 +0100

    Replace default values with "..." to satisfy Flake8

--- numpy-stubs/core/numeric.pyi
+++ numpy-stubs/core/numeric.pyi
@@ -13,63 +13,63 @@ from typing import (
 
 from numpy import ndarray, dtype, _ArrayLike, _ShapeLike
 
-T = TypeVar("T")
+_T = TypeVar("_T")
 
 def zeros_like(
     a: _ArrayLike,
-    dtype: Optional[dtype] = None,
-    order: str = "K",
-    subok: bool = True,
-    shape: Optional[Union[int, Sequence[int]]] = None,
+    dtype: Optional[dtype] = ...,
+    order: str = ...,
+    subok: bool = ...,
+    shape: Optional[Union[int, Sequence[int]]] = ...,
 ) -> ndarray[int]: ...
 def ones(
-    shape: _ShapeLike, dtype: Optional[dtype] = None, order: str = "C"
+    shape: _ShapeLike, dtype: Optional[dtype] = ..., order: str = ...
 ) -> ndarray[int]: ...
 def ones_like(
     a: _ArrayLike,
-    dtype: Optional[dtype] = None,
-    order: str = "K",
-    subok: bool = True,
-    shape: Optional[_ShapeLike] = None,
+    dtype: Optional[dtype] = ...,
+    order: str = ...,
+    subok: bool = ...,
+    shape: Optional[_ShapeLike] = ...,
 ) -> ndarray[int]: ...
 def full(
-    shape: _ShapeLike, fill_value: T, dtype: Optional[dtype] = None, order: str = "C"
-) -> ndarray[T]: ...
+    shape: _ShapeLike, fill_value: _T, dtype: Optional[dtype] = ..., order: str = ...
+) -> ndarray[_T]: ...
 def full_like(
     a: _ArrayLike,
-    fill_value: T,
-    dtype: Optional[dtype] = None,
-    order: str = "K",
-    subok: bool = True,
-    shape: Optional[_ShapeLike] = None,
-) -> ndarray[T]: ...
+    fill_value: _T,
+    dtype: Optional[dtype] = ...,
+    order: str = ...,
+    subok: bool = ...,
+    shape: Optional[_ShapeLike] = ...,
+) -> ndarray[_T]: ...
 def count_nonzero(
-    a: _ArrayLike, axis: Optional[Union[int, Tuple[int], Tuple[int, int]]] = None
+    a: _ArrayLike, axis: Optional[Union[int, Tuple[int], Tuple[int, int]]] = ...
 ) -> Union[int, ndarray[int]]: ...
 def isfortran(a: ndarray) -> bool: ...
 def argwhere(a: _ArrayLike) -> ndarray: ...
 def flatnonzero(a: _ArrayLike) -> ndarray: ...
-def correlate(a: _ArrayLike, v: _ArrayLike, mode: str = "valid") -> ndarray: ...
-def convolve(a: _ArrayLike, v: _ArrayLike, mode: str = "full") -> ndarray: ...
-def outer(a: _ArrayLike, b: _ArrayLike, out: ndarray = None) -> ndarray: ...
+def correlate(a: _ArrayLike, v: _ArrayLike, mode: str = ...) -> ndarray: ...
+def convolve(a: _ArrayLike, v: _ArrayLike, mode: str = ...) -> ndarray: ...
+def outer(a: _ArrayLike, b: _ArrayLike, out: ndarray = ...) -> ndarray: ...
 def tensordot(
     a: _ArrayLike,
     b: _ArrayLike,
     axes: Union[
         int, Tuple[int, int], Tuple[Tuple[int, int], ...], Tuple[List[int, int], ...]
-    ] = 2,
+    ] = ...,
 ) -> ndarray: ...
 def roll(
     a: _ArrayLike,
     shift: Union[int, Tuple[int, ...]],
-    axis: Optional[Union[int, Tuple[int, ...]]] = None,
-) -> T: ...
-def rollaxis(a: _ArrayLike, axis: int, start: int = 0) -> ndarray: ...
+    axis: Optional[Union[int, Tuple[int, ...]]] = ...,
+) -> _T: ...
+def rollaxis(a: _ArrayLike, axis: int, start: int = ...) -> ndarray: ...
 def normalize_axis_tuple(
     axis: Union[int, Iterable[int]],
     ndim: int,
-    argname: Optional[str] = None,
-    allow_duplicate: bool = False,
+    argname: Optional[str] = ...,
+    allow_duplicate: bool = ...,
 ) -> Tuple[int, ...]: ...
 def moveaxis(
     a: ndarray,
@@ -79,32 +79,32 @@ def moveaxis(
 def cross(
     a: _ArrayLike,
     b: _ArrayLike,
-    axisa: int = -1,
-    axisb: int = -1,
-    axisc: int = -1,
-    axis: Optional[int] = None,
+    axisa: int = ...,
+    axisb: int = ...,
+    axisc: int = ...,
+    axis: Optional[int] = ...,
 ) -> ndarray: ...
 def indices(
-    dimensions: Sequence[int], dtype: dtype = int, sparse: bool = False
+    dimensions: Sequence[int], dtype: dtype = ..., sparse: bool = ...
 ) -> Union[ndarray, Tuple[ndarray, ...]]: ...
 def fromfunction(function: Callable, shape: Tuple[int, int], **kwargs) -> Any: ...
 def isscalar(element: Any) -> bool: ...
-def binary_repr(num: int, width: Optional[int] = None) -> str: ...
-def base_repr(number: int, base: int = 2, padding: int = 0) -> str: ...
-def identity(n: int, dtype: Optional[dtype] = None) -> ndarray: ...
+def binary_repr(num: int, width: Optional[int] = ...) -> str: ...
+def base_repr(number: int, base: int = ..., padding: int = ...) -> str: ...
+def identity(n: int, dtype: Optional[dtype] = ...) -> ndarray: ...
 def allclose(
     a: _ArrayLike,
     b: _ArrayLike,
-    rtol: float = 1.0e-5,
-    atol: float = 1.0e-8,
-    equal_nan: bool = False,
+    rtol: float = ...,
+    atol: float = ...,
+    equal_nan: bool = ...,
 ) -> bool: ...
 def isclose(
     a: _ArrayLike,
     b: _ArrayLike,
-    rtol: float = 1.0e-5,
-    atol: float = 1.0e-8,
-    equal_nan: bool = False,
+    rtol: float = ...,
+    atol: float = ...,
+    equal_nan: bool = ...,
 ) -> _ArrayLike: ...
 def array_equal(a1: _ArrayLike, a2: _ArrayLike) -> bool: ...
 def array_equiv(a1: _ArrayLike, a2: _ArrayLike) -> bool: ...
