ac0ce25616ee4e41dd71cab959f1f67f94af5ead
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-18 21:44:34 +0200

    Addressed comments from https://github.com/numpy/numpy-stubs/pull/57
    * Removed ``warnings`` (https://github.com/numpy/numpy-stubs/pull/57#discussion_r410731135)
    * Deleted a now redundant comment (https://github.com/numpy/numpy-stubs/pull/57#discussion_r410732818)
    * Created and/or moved a number of tests to fail/ and reveal/ (https://github.com/numpy/numpy-stubs/pull/57#discussion_r410731679 and https://github.com/numpy/numpy-stubs/pull/57#discussion_r410731884)
    * Formatted the main __init__.pyi file with black (https://github.com/numpy/numpy-stubs/pull/57#pullrequestreview-395937631)

--- tests/pass/warnings_and_errors.py
+++ tests/pass/warnings_and_errors.py
@@ -1,18 +1,7 @@
-import types
-from typing import Type
-
 import numpy as np
-
-warnings = np.warnings  # type: types.ModuleType
-ModuleDeprecationWarning = np.ModuleDeprecationWarning  # type: Type[DeprecationWarning]
-VisibleDeprecationWarning = np.VisibleDeprecationWarning  # type: Type[UserWarning]
-ComplexWarning = np.ComplexWarning  # type: Type[RuntimeWarning]
-RankWarning = np.RankWarning  # type: Type[UserWarning]
-
-TooHardError = np.TooHardError  # type: Type[RuntimeError]
-AxisError1 = np.AxisError  # type: Type[ValueError]
-AxisError2 = np.AxisError  # type: Type[IndexError]
 
 np.AxisError(1)
 np.AxisError(1, ndim=2)
+np.AxisError(1, ndim=None)
 np.AxisError(1, ndim=2, msg_prefix='error')
+np.AxisError(1, ndim=2, msg_prefix=None)
